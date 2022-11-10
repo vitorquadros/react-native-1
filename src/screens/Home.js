6d6fc38e3e409abadde4ae7a36f9210139f0e145
Aluno <aluno@example.com>
2022-11-10 11:02:28 -0300

    init

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import MyButton from '../componentes/MyButton';
+import auth from '@react-native-firebase/auth';
+import {CommonActions} from '@react-navigation/native';
 
-const Home = () => {
+const Home = ({navigation}) => {
   const [cont, setCont] = useState(0);
 
   //exemplos de controle de ciclo de vida
@@ -27,11 +29,24 @@ const Home = () => {
     setCont(cont - 1);
   };
 
+  function signout() {
+    auth()
+      .signOut()
+      .then(() => console.log('Deslogado!'));
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [{name: 'AppStack'}],
+      }),
+    );
+  }
+
   return (
     <View>
       <Text style={styles.texto}>Contador= {cont}</Text>
       <MyButton onClick={incrementar} texto="incrementar" textoX="fasdfs" />
       <MyButton onClick={decrementar} texto="decrementar" />
+      <MyButton onClick={signout} texto="Deslogar" />
     </View>
   );
 };
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 // import { Container } from './styles';
 
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log('User account created & signed in!');
+      alert('Cadastrado com sucesso');
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
+
+      if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+
+      console.error(error);
+    });
+
   return (
     <View>
       <Text>SignUp</Text>
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        returnKeyType="next"
+        onChangeText={t => setEmail(t)}
+      />
+      <TextInput
+        placeholder="Senha"
+        keyboardType="default"
+        returnKeyType="go"
+        onChangeText={t => setPassword(t)}
+      />
     </View>
   );
 };
